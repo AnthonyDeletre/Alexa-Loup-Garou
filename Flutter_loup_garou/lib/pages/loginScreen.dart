52a10388b59a6673db394118bfda7c05c4324f02
AnthonyDeletre <anthony.deletre@gmail.com>
2020-02-10 15:20:25 +0100

    début de login screen

--- Flutter_loup_garou/lib/pages/loginScreen.dart
+++ Flutter_loup_garou/lib/pages/loginScreen.dart
@@ -7,19 +7,104 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+
+  final _formKey = GlobalKey<FormState>();
   
   @override
   Widget build(BuildContext context){
     return Scaffold(
       backgroundColor: Color.fromRGBO(56, 36, 131, 1.0),
       body: Container(
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage("assets/images/background.png"),
-            fit: BoxFit.cover,
-          ),
-        ),
-        //child: TODO
+        child: ListView(
+          children: <Widget>[
+            Row(
+            children: <Widget>[
+              Expanded(
+                flex: 1,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Container(
+                      padding: EdgeInsets.only(left: 20.0, right: 20.0, top: 20.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Image(
+                            image: AssetImage("assets/images/background.png"),
+                            fit: BoxFit.cover
+                          ),
+                          SizedBox(height: 50.0),
+                          Form(
+                            key: _formKey,
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                                TextFormField(
+                                  decoration: InputDecoration(
+                                    labelText: 'Numéro de partie',
+                                    labelStyle: TextStyle(
+                                      color: Colors.white
+                                    ),
+                                    fillColor: Colors.white,
+                                    enabledBorder: OutlineInputBorder(
+                                      borderSide: BorderSide(color: Colors.white, width: 1.0),
+                                      borderRadius: BorderRadius.circular(25.0),
+                                    ),
+                                    focusedBorder: OutlineInputBorder(
+                                      borderSide: BorderSide(color: Colors.white, width: 1.0),
+                                      borderRadius: BorderRadius.circular(25.0),
+                                    ),
+                                  ),
+                                  validator: (value) {
+                                    if (value.isEmpty) {
+                                      return 'Veuillez entrer un numéro de partie';
+                                    }
+                                    return null;
+                                  },
+                                ),
+                                SizedBox(height: 50.0),
+                                FadeInState(
+                                child: Container(
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(50),
+                                    color:  Color.fromRGBO(246, 187, 28, 1.0),
+                                  ),
+                                  height: 50.0,
+                                  child: InkWell(
+                                    onTap: () {
+                                      if (_formKey.currentState.validate()) {
+                                        Scaffold.of(context).showSnackBar(SnackBar(content: Text('Recherche de la partie')));
+                                      }
+                                    },
+                                    child: Padding(
+                                      padding: EdgeInsets.all(10),
+                                      child: Center(
+                                        child: Text(
+                                          'Envoyer',
+                                          style: TextStyle(
+                                            color: Colors.white,
+                                            fontSize: 16,
+                                            fontWeight: FontWeight.bold
+                                          ),
+                                        ),
+                                      )
+                                    )
+                                  )
+                                ),
+                              ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                )
+              )
+            ],
+          )
+          ],
+        )
       ),
     );
   }
