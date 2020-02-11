46446ce8009216ad1e63f1c3f53060c88d006589
AnthonyDeletre <anthony.deletre@gmail.com>
2020-02-11 15:22:16 +0100

    Lobby page

--- Flutter_loup_garou/lib/data.dart
+++ Flutter_loup_garou/lib/data.dart
@@ -11,7 +11,12 @@ List<Player> getListPlayer(){
   final List<Player> list = new List();
 
   list.add(new Player(1, 'Joueur 1'));
-
+  list.add(new Player(2, 'Joueur 2'));
+  list.add(new Player(3, 'Joueur 3'));
+  list.add(new Player(4, 'Joueur 4'));
+  list.add(new Player(5, 'Joueur 5'));
+  list.add(new Player(6, 'Joueur 6'));
+  list.add(new Player(7, 'Joueur 7'));
 
   return list;
 }
--- Flutter_loup_garou/lib/pages/lobbyScreen.dart
+++ Flutter_loup_garou/lib/pages/lobbyScreen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter_loup_garou/animations/fadeInState.dart';
 import 'package:flutter_loup_garou/api.dart';
 import 'package:flutter_loup_garou/data.dart';
+import 'package:flutter_loup_garou/pages/gameScreen.dart';
 import 'package:flutter/material.dart';
 
 class LobbyScreen extends StatefulWidget {
@@ -28,71 +29,79 @@ class _LobbyScreenState extends State<LobbyScreen> {
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
                   Container(
-                    padding: EdgeInsets.only(left: 20.0, right: 20.0, top: 50.0),
+                    padding: EdgeInsets.only(top: 50.0),
                     child: Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: <Widget>[
-                        Center(
-                        child: Text(
-                          "Salon",
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 25.0
-                            )
-                          ),
+                        FadeInState(
+                          child: Center(
+                            child: Text(
+                              "Salon",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 25.0
+                                )
+                              ),
+                            ),
                         ),
                         Padding(
-                        padding: EdgeInsets.only(top: 50.0),
+                        padding: EdgeInsets.only(top: 20.0),
                         child: Column(
                           mainAxisSize: MainAxisSize.min,
                           mainAxisAlignment: MainAxisAlignment.start,
                           crossAxisAlignment: CrossAxisAlignment.start,
                           children: <Widget>[
                             Container(
-                              height: MediaQuery.of(context).size.height - 300.0,
+                              height: MediaQuery.of(context).size.height - 100.0,
                               decoration: BoxDecoration(
                                 color: Colors.white,
                                 borderRadius: BorderRadius.only(topLeft: Radius.circular(40.0), topRight: Radius.circular(40.0))
                               ),
-                              child:ListView.builder(
-                                shrinkWrap: true,
-                                itemCount: players.length,
-                                itemBuilder: (context, index) {
-                                    return _listItem(context, index);            
-                                }
-                              ),
-                            ),
-                            Container(
-                              child: Padding(
-                                padding: EdgeInsets.only(top: 3, bottom: 3),
-                                child: InkWell(
-                                  onTap: () {
-                                    // Navigator.push(
-                                    //   context,
-                                    //   MaterialPageRoute(builder: (context) => FormScreen()),
-                                    // );
-                                  },
-                                  child: Container(
-                                    decoration: BoxDecoration(
-                                    borderRadius: BorderRadius.all(Radius.circular(40.0)),
-                                    color: Color.fromRGBO(246, 187, 28, 1.0)
-                                    ),
-                                    height: 50.0,
-                                    child: Center(
-                                      child: Text(
-                                        'Prêt',
-                                        textAlign: TextAlign.center,
-                                        style: TextStyle(
-                                        color: Colors.white,
-                                        fontSize: 16,
-                                        )
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                children: <Widget>[
+                                  SizedBox(height: 20.0),
+                                  ListView.builder(
+                                    shrinkWrap: true,
+                                    itemCount: players.length,
+                                    itemBuilder: (context, index) {
+                                        return _listItem(context, index);            
+                                    }
+                                  ),
+                                  Container(
+                                    child: Padding(
+                                      padding: EdgeInsets.only(top: 40.0, left: 20.0, right: 20.0, bottom: 50.0),
+                                      child: InkWell(
+                                        onTap: () {
+                                          Navigator.push(
+                                            context,
+                                            MaterialPageRoute(builder: (context) => GameScreen()),
+                                          );
+                                        },
+                                        child: Container(
+                                          decoration: BoxDecoration(
+                                          borderRadius: BorderRadius.all(Radius.circular(40.0)),
+                                          color: Color.fromRGBO(246, 187, 28, 1.0)
+                                          ),
+                                          height: 50.0,
+                                          child: Center(
+                                            child: Text(
+                                              'Prêt',
+                                              textAlign: TextAlign.center,
+                                              style: TextStyle(
+                                              color: Colors.white,
+                                              fontSize: 16,
+                                              )
+                                            ),
+                                          ),
+                                        ),
                                       ),
                                     ),
                                   ),
-                                ),
-                              ),
-                            ), 
+                                ],
+                              )
+                            ),
                           ]
                         ),
                       )
@@ -115,29 +124,41 @@ class _LobbyScreenState extends State<LobbyScreen> {
     String name = players[index].name; 
 
     return Padding(
-      padding: EdgeInsets.only(left: 10.0, right: 10.0, top: 15.0, bottom: 15.0),
+      padding: EdgeInsets.only(top: 20.0),
       child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: <Widget>[
-            Expanded(flex: 6,
+            Expanded(flex: 1,
             child: 
               Container(
               child: Row(
                 children: [
-                  Expanded(flex: 1,
+                  Expanded(
+                    flex: 1,
                     child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        name,
-                        style: TextStyle(
-                          fontSize: 16.0,
-                          fontWeight: FontWeight.bold
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        FadeInState(
+                          child: Center(
+                            child: Text(
+                              name,
+                              style: TextStyle(
+                                fontSize: 16.0,
+                                fontWeight: FontWeight.bold
+                              ),
+                            ),
+                          )
                         ),
-                      ),
-                    ],
-                  ),
-                  )     
+                        SizedBox(height: 20.0),
+                        Divider(
+                          height: 5.0, 
+                          color: Color.fromRGBO(56, 36, 131, 1.0), 
+                          thickness: 1.0, 
+                          indent: 60.0,
+                          endIndent: 60.0,
+                        )
+                      ],
+                    ),
+                  )
                 ],
               ),
             ),
